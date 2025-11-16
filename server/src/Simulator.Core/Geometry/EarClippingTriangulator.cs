68c4c5ba2e74359e351ade1dccb2b58bf5f8b85b
Sam Feast <sfeast43@gmail.com>
2025-11-16 18:19:24 +0000

    add triangulation clipping loop

--- server/src/Simulator.Core/Geometry/EarClippingTriangulator.cs
+++ server/src/Simulator.Core/Geometry/EarClippingTriangulator.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using Simulator.Core.Geometry.Primitives;
 
 namespace Simulator.Core.Geometry;
@@ -6,9 +7,9 @@ public class EarClippingTriangulator : ITriangulator
 {
 
     private LinkedList<Vector2Int> _vertices = [];
-    private List<LinkedListNode<Vector2Int>> _convexVertices = [];
-    private List<LinkedListNode<Vector2Int>> _reflexVertices = [];
-    private List<LinkedListNode<Vector2Int>> _earVertices = [];
+    private HashSet<LinkedListNode<Vector2Int>> _convexVertices = [];
+    private HashSet<LinkedListNode<Vector2Int>> _reflexVertices = [];
+    private HashSet<LinkedListNode<Vector2Int>> _earVertices = [];
     public List<Triangle> Triangulate(List<Polygon> polygons) 
     {
         // Construct doubly linked list with vertices in CCW order
@@ -16,55 +17,31 @@ public class EarClippingTriangulator : ITriangulator
         _vertices = BuildVertexList(polygons);
 
         // Handle edge cases where a triangle cannot be formed, or only one triangle can be formed
-        if (_vertices.Count < 3) return [];
-
+        if (_vertices.Count < 3) 
+            return [];
         if (_vertices.Count == 3)
-        {
-            var v0 = _vertices.First!.Value;
-            var v1 = _vertices.First.Next!.Value;
-            var v2 = _vertices.Last!.Value;
-            return [ new Triangle(v0, v1, v2) { direction = Direction.CCW } ];
-        }
+            return [ GetLastTriangle() ];
         
         ClassifyVertices();
-
-        foreach (var v in _convexVertices)
-        {
-            Console.WriteLine($"Convex: {v.Value}");
-        }
-        foreach (var v in _reflexVertices)
-        {
-            Console.WriteLine($"Reflex: {v.Value}");
-        }
-        
         ComputeEars();
         
-        foreach (var v in _earVertices)
+        List<Triangle> triangles = [];
+        
+        // Clipping loop
+        while (_vertices.Count > 3)
         {
-            Console.WriteLine($"Ear: {v.Value}");
+            var ear = _earVertices.First();
+            var triangle = ClipEar(ear);
+            triangles.Add(triangle);
         }
         
-        /*
+        triangles.Add(GetLastTriangle());
         
-        var node = _vertices.First;
-        while (node != null)
-        {
-            if (IsEar(node))
-            {
-                Console.WriteLine($"{node.Value} is an ear");
-            }
-            else
-            {
-                Console.WriteLine($"{node.Value} is not an ear");
-            }
-
-            node = node.Next;
-        }
-        */
-        return [];
+        return triangles;
     }
-
-    private LinkedList<Vector2Int> BuildVertexList(List<Polygon> polygons)
+    
+    // Take list of input polygons and return correctly ordered vertex list
+    private static LinkedList<Vector2Int> BuildVertexList(List<Polygon> polygons)
     {
         var vertices = new LinkedList<Vector2Int>();
         foreach (var polygon in polygons)
@@ -86,23 +63,37 @@ public class EarClippingTriangulator : ITriangulator
         return vertices;
     }
 
+    // Group all vertices into the convex and reflex sets
     private void ClassifyVertices()
     {
         var node = _vertices.First;
         while (node != null)
         {
-            var prevNode = node.Previous ?? _vertices.Last;
-            var nextNode = node.Next ?? _vertices.First;
-            
-            if (Cross(prevNode!.Value, node!.Value, nextNode!.Value) > 0)
-                _convexVertices.Add(node);
-            else
-                _reflexVertices.Add(node);
-
+            ClassifyVertex(node);
             node = node.Next;
         }
     }
 
+    // Ensure the specified vertex is in exactly one of the convex/reflex sets
+    // Returns true if the vertex is convex, false otherwise
+    private bool ClassifyVertex(LinkedListNode<Vector2Int> vertex)
+    {
+        var (prevNode, nextNode) = GetNeighbours(vertex);
+        if (Cross(prevNode.Value, vertex.Value, nextNode.Value) > 0)
+        {
+            _convexVertices.Add(vertex);
+            _reflexVertices.Remove(vertex);
+            return true;
+        }
+
+        _reflexVertices.Add(vertex);
+        _convexVertices.Remove(vertex);
+        return false;
+
+    }
+    
+    // Add ear vertices to the set
+    // NOTE: Only call this when _convexVertices is up to date
     private void ComputeEars()
     {
         foreach (var node in _convexVertices)
@@ -112,17 +103,14 @@ public class EarClippingTriangulator : ITriangulator
         }
     }
     
+    // Check if a given vertex is an ear
     private bool IsEar(LinkedListNode<Vector2Int> tipNode)
     {
-        // Get the previous and next nodes in a circular manner
-        var prevNode = tipNode.Previous ?? _vertices.Last;
-        var nextNode = tipNode.Next ?? _vertices.First;
-        
+        var (prevNode, nextNode) = GetNeighbours(tipNode);
 
-        var triangle = new Triangle(prevNode!.Value, tipNode.Value, nextNode!.Value);
+        var triangle = new Triangle(prevNode.Value, tipNode.Value, nextNode.Value);
 
         // Now check if any reflex vertices are in the triangle created by the three vertices
-
         foreach (var node in _reflexVertices)
         {
             if (node == prevNode || node == tipNode || node == nextNode)
@@ -133,8 +121,53 @@ public class EarClippingTriangulator : ITriangulator
         
         return true;
     }
+
+    // Single iteration of clipping loop
+    private Triangle ClipEar(LinkedListNode<Vector2Int> earNode)
+    {
+        var (prevNode, nextNode) = GetNeighbours(earNode);
+        var triangle = new Triangle(prevNode.Value, earNode.Value, nextNode.Value);
+
+        _vertices.Remove(earNode);
+        _convexVertices.Remove(earNode);
+        _reflexVertices.Remove(earNode);
+        _earVertices.Remove(earNode);
+
+        // Classify the previous node as convex/reflex
+        // If its convex, check if it's an ear and update the ear set
+        if (ClassifyVertex(prevNode) && IsEar(prevNode))
+            _earVertices.Add(prevNode);
+        else
+            _earVertices.Remove(prevNode);
+        
+        if (ClassifyVertex(nextNode) && IsEar(nextNode))
+            _earVertices.Add(nextNode);
+        else
+            _earVertices.Remove(nextNode);
+
+        return triangle;
+    }
+
+    // Get the previous and next nodes in a circular manner
+    private (LinkedListNode<Vector2Int>, LinkedListNode<Vector2Int>) GetNeighbours(LinkedListNode<Vector2Int> node)
+    {
+        Debug.Assert(_vertices.Count > 0, "Tried to get neighbours for empty vertex list");
+        var prevNode = node.Previous ?? _vertices.Last;
+        var nextNode = node.Next ?? _vertices.First;
+
+        return (prevNode!, nextNode!);
+    }
     
-    private int Cross(Vector2Int v0, Vector2Int v1, Vector2Int v2)
+    // Get the triangle formed by the first, second, and last elements in the vertices list
+    // Only to be used when there are exactly three vertices in the list
+    private Triangle GetLastTriangle()
+    {
+        Debug.Assert(_vertices.Count == 3, "Tried to get last triangle when _vertices.Count != 3");
+        return new Triangle(_vertices.First!.Value, _vertices.First.Next!.Value, _vertices.Last!.Value);
+    }
+    
+    // 2D cross product to determine convexity
+    private static int Cross(Vector2Int v0, Vector2Int v1, Vector2Int v2)
     {
         return (v1.X - v0.X)*(v2.Y - v1.Y) - (v1.Y - v0.Y)*(v2.X - v1.X);
     }
