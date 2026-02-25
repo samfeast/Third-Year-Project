a913218d4938085d2fd6f5bc1ad49536943d3dca
Sam Feast <sfeast43@gmail.com>
2026-02-25 09:44:15 +0000

    update vertices.json + add crude converter to presetN.ts

---
+++ scripts/src/vertices_to_floorplan.py
@@ -0,0 +1,31 @@
+import json
+
+
+def get_lines(vertices):
+    lines = []
+
+    for i in range(len(vertices)):
+        j = (i + 1) % len(vertices)
+        lines.append(
+            [
+                vertices[i][0],
+                vertices[i][1],
+                vertices[j][0],
+                vertices[j][1],
+            ]
+        )
+
+    return lines
+
+
+with open("../data/vertices.json", "r") as read_file:
+    data = json.load(read_file)
+
+flattened_lines = []
+
+flattened_lines += get_lines(data["positive"])
+
+for negative in data["negatives"]:
+    flattened_lines += get_lines(negative)
+
+print(flattened_lines)
