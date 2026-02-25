e060cb4795e98a5b704b53991a92116a9e835c4d
Sam Feast <sfeast43@gmail.com>
2026-02-25 09:42:49 +0000

    process preset messages on server

--- server/src/Simulator.Server/Program.cs
+++ server/src/Simulator.Server/Program.cs
@@ -1,10 +1,20 @@
 using System.Net.WebSockets;
 using System.Text;
+using System.Text.Json;
 using Simulator.Core;
+using Simulator.Core.Geometry.Primitives;
+using Simulator.Core.Geometry.Utils;
+using Simulator.IO;
 using Simulator.IO.Json;
 using Simulator.Server.ManagerCommands;
 
 namespace Simulator.Server;
+
+public class ClientMessage
+{
+    public required string command { get; set; }
+    public int preset { get; set; }
+}
 
 public class Program
 {
@@ -35,7 +45,8 @@ public class Program
         var manager = app.Services.GetRequiredService<SimulationManager>();
         manager.SnapshotProduced += (id, snapshot) =>
         {
-            Console.WriteLine($"[{DateTime.Now:HH:mm:ss.fff}] Sim {id} Tick {snapshot.Step}");
+            if (snapshot.Step % 100 == 0 || snapshot.AllComplete)
+                Console.WriteLine($"[{DateTime.Now:HH:mm:ss.fff}] Sim {id} Tick {snapshot.Step}");
         };
 
         app.MapControllers();
@@ -68,12 +79,45 @@ public class Program
 
                 if (result.MessageType == WebSocketMessageType.Text)
                 {
-                    var message = Encoding.UTF8.GetString(buffer, 0, result.Count);
+                    var messageJson = Encoding.UTF8.GetString(buffer, 0, result.Count);
+                    Console.WriteLine($"Received from client: {messageJson}");
+                    var message = JsonSerializer.Deserialize<ClientMessage>(messageJson);
 
-                    Console.WriteLine($"Received from client: {message}");
-                    if (message == "create")
+
+                    if (message == null) continue;
+
+                    // Crude message processing for hardcoded presets
+                    if (message is { command: "create", preset: 1 })
                     {
                         manager.EnqueueCommand(new CreateSimulationCommand(clientId, SimulationConfig.Preset1, 0));
+                    }
+                    if (message is { command: "create", preset: 2 })
+                    {
+                        var readRegistry = DeserialiserRegistryFactory.Default<InputGeometry>();
+                        
+                        var inputGeometry = readRegistry.Load("../../../scripts/data/vertices2.json");
+                        var config = new SimulationConfig {
+                            Geometry = inputGeometry,
+                            TimeStep = 0.1f,
+                            NumAgents = 50,
+                            Seed = 101,
+                            Target = new Vector2(250, 250)
+                        };
+                        manager.EnqueueCommand(new CreateSimulationCommand(clientId, config, 0));
+                    }
+                    
+                    if (message is { command: "create", preset: 3 })
+                    {
+                        var readRegistry = DeserialiserRegistryFactory.Default<InputGeometry>();
+                        
+                        var inputGeometry = readRegistry.Load("../../../scripts/data/vertices.json");
+                        var config = new SimulationConfig {
+                            Geometry = inputGeometry,
+                            TimeStep = 0.1f,
+                            NumAgents = 20,
+                            Seed = 102,
+                        };
+                        manager.EnqueueCommand(new CreateSimulationCommand(clientId, config, 0));
                     }
                 }
             }
@@ -86,8 +130,6 @@ public class Program
             void SendSnapshots(Guid id, SimulationSnapshot snapshot)
             {
                 if (webSocket.State != WebSocketState.Open) return;
-                
-                Console.WriteLine($"{snapshot.Positions.Length} positions");
 
                 var serialiser = new JsonSimulationSnapshotSerialiser();
                 var data = serialiser.Serialise(snapshot, 1);
