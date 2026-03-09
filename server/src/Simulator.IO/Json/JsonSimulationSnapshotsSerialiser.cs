7baa9fdd49c70fa317fb9b9531b567f6e2b15b54
Sam Feast <sfeast43@gmail.com>
2026-03-09 02:26:04 +0000

    add v2 json simulation snapshots serialiser

--- server/src/Simulator.IO/Json/JsonSimulationSnapshotsSerialiser.cs
+++ server/src/Simulator.IO/Json/JsonSimulationSnapshotsSerialiser.cs
@@ -8,7 +8,7 @@ internal class JsonSimulationSnapshotsSerialiser : ISerialiser<List<SimulationSn
 {
     public bool CanWrite(DataFormat format, DataType type, int version)
     {
-        return format == DataFormat.JSON && type == DataType.SimulationSnapshots && version is >= 1 and <= 1;
+        return format == DataFormat.JSON && type == DataType.SimulationSnapshots && version is >= 1 and <= 2;
     }
     
     public byte[] Serialise(List<SimulationSnapshot> snapshots, int version)
@@ -16,6 +16,7 @@ internal class JsonSimulationSnapshotsSerialiser : ISerialiser<List<SimulationSn
         return version switch
         {
             1 => SerialiseV1(snapshots),
+            2 => SerialiseV2(snapshots),
             _ => throw new NotImplementedException(
                 $"JSON simulation snapshots serialiser does not implement version {version}")
         };
@@ -26,6 +27,34 @@ internal class JsonSimulationSnapshotsSerialiser : ISerialiser<List<SimulationSn
         var data = new
         {
             version = 1,
+            snapshots = snapshots.Select(snapshot => new
+            {
+                step = snapshot.Step,
+                final = snapshot.AllComplete,
+                agents = snapshot.Ids
+                    .Select((id, i) => new
+                    {
+                        id,
+                        position = new[]
+                        {
+                            snapshot.Positions[i].X,
+                            snapshot.Positions[i].Y
+                        },
+                        speed = snapshot.Speeds[i]
+                    })
+            })
+        };
+        
+        return JsonSerializer.SerializeToUtf8Bytes(data);
+    }
+
+    private byte[] SerialiseV2(List<SimulationSnapshot> snapshots)
+    {
+        var data = new
+        {
+            type = "snapshots",
+            version = 2,
+            numSnapshots = snapshots.Count,
             snapshots = snapshots.Select(snapshot => new
             {
                 step = snapshot.Step,
